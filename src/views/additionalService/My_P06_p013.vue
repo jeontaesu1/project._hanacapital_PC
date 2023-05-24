9578a45f19a47c6d5ab35855e84710f8ccfa2b6d
kim tae kyun <nonoday@mwkorea.co.kr>
2023-05-24 11:08:10 +0900

    온라인약정_약정안내 추가

---
+++ src/views/additionalService/My_P06_p013.vue
@@ -0,0 +1,67 @@
+<script>
+// My_P06_p013
+import PageContents from '@/components/ui/layout/PageContents.vue';
+import PageHead from '@/components/ui/text/PageHead.vue';
+import PageTitle from '@/components/ui/text/PageTitle.vue';
+import BasicBox from '@/components/ui/common/BasicBox.vue';
+import IconStar from '@/assets/images/icon/star-badge.svg?component';
+import IllustObject from '@/components/ui/common/IllustObject.vue';
+import BasicButton from '@/components/ui/button/BasicButton.vue';
+import ButtonList from '@/components/ui/button/ButtonList.vue';
+import ButtonListItem from '@/components/ui/button/ButtonListItem.vue';
+
+export default {
+  components: {
+    PageContents,
+    PageHead,
+    PageTitle,
+    BasicBox,
+    IconStar,
+    IllustObject,
+    BasicButton,
+    ButtonList,
+    ButtonListItem,
+  },
+};
+</script>
+
+<template>
+  <PageContents>
+    <PageHead>
+      <PageTitle
+        >지금부터 하나캐피탈 이용을 위한<br />온라인 약정을
+        진행합니다</PageTitle
+      >
+    </PageHead>
+    <IllustObject type="certification" />
+    <BasicBox>
+      <div :class="$style['icon-box']">
+        <div :class="$style['icon-box__icon']">
+          <IconStar />
+        </div>
+        <p
+          :class="[
+            $style['icon-box__content'],
+            'text-title-1',
+            'row-margin-item',
+          ]"
+        >
+          <span class="color-green font-weight-bold">하나프라자</span>님은
+          하나캐피탈에 등록된<br />
+          <span class="font-weight-bold">정식 모집인</span>입니다
+        </p>
+      </div>
+    </BasicBox>
+    <ButtonList>
+      <ButtonListItem>
+        <BasicButton :line="true" theme="quaternary">이전</BasicButton>
+      </ButtonListItem>
+      <ButtonListItem>
+        <BasicButton>확인</BasicButton>
+      </ButtonListItem>
+    </ButtonList>
+  </PageContents>
+</template>
+<style lang="scss" module>
+@import '@/assets/scss/views/additionalService/My_P06_p013.scss';
+</style>
