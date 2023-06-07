cb13464d4ba5996ac031b01adf9f812ca51e93de
yoondowon <yoondw7832@mwkorea.co.kr>
2023-06-07 15:24:20 +0900

    download-icon 추가

--- src/views/uiGuide/GuideComponent.vue
+++ src/views/uiGuide/GuideComponent.vue
@@ -90,6 +90,7 @@ import IconContract from '@/assets/images/icon/contract.svg?component';
 import IconCarCheck from '@/assets/images/icon/car-check.svg?component';
 import IconCar from '@/assets/images/icon/car.svg?component';
 import IconDocumentSearch from '@/assets/images/icon/document-search.svg?component';
+import IconDownload from '@/assets/images/icon/download.svg?component';
 
 export default {
   components: {
@@ -181,6 +182,7 @@ export default {
     IconCarCheck,
     IconCar,
     IconDocumentSearch,
+    IconDownload,
   },
 
   setup() {
@@ -731,6 +733,12 @@ export default {
           <template v-slot:rightIcon>
             <IconAdd />
           </template>
+        </BasicButton>
+        <BasicButton tagName="a" inline="true" size="regular">
+          <template v-slot:leftIcon>
+            <IconDownload />
+          </template>
+          Button
         </BasicButton>
       </div>
 
