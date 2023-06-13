d4df5c3fee51690ebfde7eec34fec5b428a3adcb
leechaehyun <leechaehyun@mwkorea.co.kr>
2023-06-13 15:42:50 +0900

    ===작업중=== 손님용 > 회사소개 > 연혁

--- src/views/company/Company_P01_p003.vue
+++ src/views/company/Company_P01_p003.vue
@@ -1,24 +1,99 @@
 <script>
 // Company_P01_p003
 import PageContents from '@/components/ui/layout/PageContents.vue';
+import UiTab from '@/components/ui/tab/UiTab.vue';
+import NavTab from '@/components/ui/tab/NavTab.vue';
+import NavTabButton from '@/components/ui/tab/NavTabButton.vue';
 import PageHead from '@/components/ui/text/PageHead.vue';
 import PageTitle from '@/components/ui/text/PageTitle.vue';
+import UiTabPanel from '@/components/ui/tab/UiTabPanel.vue';
+import KeyValueItem from '@/components/ui/text/KeyValueItem.vue';
+import KeyValue from '@/components/ui/text/KeyValue.vue';
+import KeyValueTitle from '@/components/ui/text/KeyValueTitle.vue';
+import KeyValueText from '@/components/ui/text/KeyValueText.vue';
 
 export default {
   components: {
     PageContents,
     PageHead,
     PageTitle,
+    UiTab,
+    NavTab,
+    NavTabButton,
+    UiTabPanel,
+    KeyValueItem,
+    KeyValue,
+    KeyValueTitle,
+    KeyValueText,
   },
 };
 </script>
 
 <template>
-  <PageContents>
+  <PageContents size="wide">
     <PageHead>
-      <PageTitle>타이틀</PageTitle>
+      <PageTitle>연혁</PageTitle>
     </PageHead>
 
-    // contents
+    <UiTab>
+      <NavTab :useUiTab="true">
+        <NavTabButton link="Company_P01_p003_navTab001">
+          도전하는 하나캐피탈 2011 ~ 현재
+        </NavTabButton>
+        <NavTabButton link="Company_P01_p003_navTab002">
+          우리의 시작 2004 ~ 2010
+        </NavTabButton>
+      </NavTab>
+
+      <UiTabPanel name="Company_P01_p003_navTab001">
+        <ul class="reset-list">
+          <li class="row-margin-container-medium">
+            <div class="flex-box align-items-start">
+              <div class="flex-box__cell">
+                <h3 class="text-title-1">2021</h3>
+              </div>
+              <div class="flex-box__cell">
+                <KeyValue align="left" margin="small">
+                  <KeyValueItem>
+                    <KeyValueTitle>7월</KeyValueTitle>
+                    <KeyValueText>유상증자 (납입자본금 1,462억)</KeyValueText>
+                  </KeyValueItem>
+                  <KeyValueItem>
+                    <KeyValueTitle>3월</KeyValueTitle>
+                    <KeyValueText>
+                      미얀마 현지법인 “Hana Microfinance Limited” 유상증자 참여
+                      (지분 75%)
+                    </KeyValueText>
+                  </KeyValueItem>
+                </KeyValue>
+              </div>
+            </div>
+          </li>
+
+          <li class="row-margin-container-medium">
+            <div class="flex-box align-items-start">
+              <div class="flex-box__cell">
+                <h3 class="text-title-1">2021</h3>
+              </div>
+              <div class="flex-box__cell">
+                <KeyValue align="left" margin="small">
+                  <KeyValueItem>
+                    <KeyValueTitle>11월</KeyValueTitle>
+                    <KeyValueText>하나원큐드림카 VER.2 오픈</KeyValueText>
+                  </KeyValueItem>
+                  <KeyValueItem>
+                    <KeyValueTitle>7월</KeyValueTitle>
+                    <KeyValueText>
+                      업권 최초 내구재 할부 영업지원 플랫폼 ‘원큐하나로‘ 오픈
+                    </KeyValueText>
+                  </KeyValueItem>
+                </KeyValue>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </UiTabPanel>
+      <UiTabPanel name="Company_P01_p003_navTab002"></UiTabPanel>
+    </UiTab>
   </PageContents>
 </template>
--- src/views/customer/Customer_P02_p002.vue
+++ src/views/customer/Customer_P02_p002.vue
@@ -1,14 +1,60 @@
 <script>
 // Customer_P02_p002
+import { reactive } from 'vue';
+
 import PageContents from '@/components/ui/layout/PageContents.vue';
+import StepProgress from '@/components/ui/progress/StepProgress.vue';
 import PageHead from '@/components/ui/text/PageHead.vue';
+import PageHeadRow from '@/components/ui/text/PageHeadRow.vue';
+import PageMainText from '@/components/ui/text/PageMainText.vue';
 import PageTitle from '@/components/ui/text/PageTitle.vue';
+import BasicButton from '@/components/ui/button/BasicButton.vue';
+import ButtonList from '@/components/ui/button/ButtonList.vue';
+import ButtonListItem from '@/components/ui/button/ButtonListItem.vue';
+import FormList from '@/components/ui/form/FormList.vue';
+import FormListItem from '@/components/ui/form/FormListItem.vue';
+import InputBlock from '@/components/ui/form/InputBlock.vue';
+import InputBlockCell from '@/components/ui/form/InputBlockCell.vue';
+import BasicSelect from '@/components/ui/form/BasicSelect.vue';
+import FormInvalid from '@/components/ui/form/FormInvalid.vue';
+import BasicInput from '@/components/ui/form/BasicInput.vue';
+import FormInvalidMessage from '@/components/ui/form/FormInvalidMessage.vue';
+import BasicTextarea from '@/components/ui/form/BasicTextarea.vue';
 
 export default {
   components: {
     PageContents,
     PageHead,
     PageTitle,
+    PageHeadRow,
+    StepProgress,
+    PageMainText,
+    ButtonList,
+    ButtonListItem,
+    BasicButton,
+    FormList,
+    FormListItem,
+    InputBlock,
+    InputBlockCell,
+    BasicSelect,
+    FormInvalid,
+    BasicInput,
+    FormInvalidMessage,
+    BasicTextarea,
+  },
+
+  setup() {
+    const state = reactive({
+      nameError: false,
+      birthNumberError: false,
+      phoneError: false,
+      mailError: false,
+      counselingError: false,
+    });
+
+    return {
+      state,
+    };
   },
 };
 </script>
@@ -16,9 +62,207 @@ export default {
 <template>
   <PageContents>
     <PageHead>
-      <PageTitle>타이틀</PageTitle>
+      <PageHeadRow>
+        <PageTitle align="left">고객상담</PageTitle>
+        <template v-slot:right>
+          <StepProgress :total="3" :current="2" />
+        </template>
+      </PageHeadRow>
+      <PageMainText align="left">
+        약관 동의 후 상담 내용을 입력해 주세요
+      </PageMainText>
     </PageHead>
 
-    // contents
+    <div>// 약관 UI 대기</div>
+
+    <section class="row-margin-block-small">
+      <h3 class="text-title-1 row-margin-contents">상담 내용 입력</h3>
+
+      <FormList>
+        <FormListItem
+          titleText="문의분야"
+          titleOptionalText="(일반문의)"
+          target="#Customer_P02_p002_type"
+          :selectOnly="true"
+        >
+          <InputBlock :disabled="true">
+            <InputBlockCell :flexible="true">
+              <BasicSelect
+                :options="[
+                  {
+                    value: '1',
+                    label: '정보 변경',
+                  },
+                  {
+                    value: '2',
+                    label: '제증명발급',
+                  },
+                  {
+                    value: '3',
+                    label: '상환문의',
+                  },
+                  {
+                    value: '4',
+                    label: '기타문의',
+                  },
+                  {
+                    value: '5',
+                    label: '홈페이지관련',
+                  },
+                  {
+                    value: '6',
+                    label: '채용관련',
+                  },
+                  {
+                    value: '7',
+                    label: '청약철회신청',
+                  },
+                ]"
+                title="문의분야"
+                inputId="Customer_P02_p002_type"
+              />
+            </InputBlockCell>
+          </InputBlock>
+        </FormListItem>
+
+        <FormListItem titleText="이름" target="#Customer_P02_p002_name">
+          <FormInvalid :error="state.nameError">
+            <InputBlock :error="state.nameError">
+              <InputBlockCell :flexible="true">
+                <BasicInput title="이름" id="Customer_P02_p002_name" />
+              </InputBlockCell>
+            </InputBlock>
+            <FormInvalidMessage>Error Message</FormInvalidMessage>
+          </FormInvalid>
+        </FormListItem>
+
+        <FormListItem titleText="생년월일" target="#Customer_P02_p002_birth">
+          <FormInvalid :error="state.birthNumberError">
+            <InputBlock :error="state.birthNumberError">
+              <InputBlockCell :flexible="true">
+                <BasicInput
+                  type="number"
+                  pattern="\d*"
+                  title="생년월일"
+                  id="Customer_P02_p002_birth"
+                />
+              </InputBlockCell>
+            </InputBlock>
+            <FormInvalidMessage>Error Message</FormInvalidMessage>
+          </FormInvalid>
+        </FormListItem>
+
+        <FormListItem titleText="연락처" target="#Customer_P02_p002_phone">
+          <FormInvalid :error="state.phoneError">
+            <InputBlock :error="state.phoneError">
+              <InputBlockCell :flexible="true">
+                <BasicInput
+                  pattern="\d*"
+                  title="연락처"
+                  id="Customer_P02_p002_phone"
+                />
+              </InputBlockCell>
+            </InputBlock>
+            <FormInvalidMessage>Error Message</FormInvalidMessage>
+          </FormInvalid>
+        </FormListItem>
+
+        <FormListItem titleText="이메일" target="#Customer_P02_p002_EmailId">
+          <FormInvalid :error="state.mailError">
+            <InputBlock :error="state.mailError">
+              <InputBlockCell :flexible="true">
+                <BasicInput
+                  title="이메일 아이디"
+                  id="Customer_P02_p002_EmailId"
+                />
+              </InputBlockCell>
+              <InputBlockCell margin="regular">@</InputBlockCell>
+              <InputBlockCell margin="regular" :flexible="true">
+                <BasicSelect
+                  :options="[
+                    {
+                      value: '1',
+                      label: 'naver.com',
+                    },
+                    {
+                      value: '2',
+                      label: 'hanmail.net',
+                    },
+                    {
+                      value: '3',
+                      label: 'gmail.com',
+                    },
+                    {
+                      value: '4',
+                      label: 'nate.com',
+                    },
+                    {
+                      value: '5',
+                      label: 'paran.com',
+                    },
+                    {
+                      value: '6',
+                      label: 'dreamwiz.com',
+                    },
+                    {
+                      value: '7',
+                      label: 'yahoo.com',
+                    },
+                    {
+                      value: '8',
+                      label: 'freechal.com',
+                    },
+                    {
+                      value: '9',
+                      label: '직접입력',
+                    },
+                  ]"
+                  title="이메일 도메인"
+                />
+              </InputBlockCell>
+            </InputBlock>
+            <!-- Case : 직접입력 선택 시 노출 -->
+            <InputBlock
+              :error="state.mailError"
+              :classNames="{
+                wrap: 'row-margin-item-group row-margin-bottom-none',
+              }"
+            >
+              <InputBlockCell :flexible="true">
+                <BasicInput
+                  title="이메일 도메인 직접입력"
+                  id="Customer_P02_p002_EmailDomainSelf"
+                />
+              </InputBlockCell>
+            </InputBlock>
+            <!-- //Case : 직접입력 선택 시 노출 -->
+            <FormInvalidMessage>Error Message</FormInvalidMessage>
+          </FormInvalid>
+        </FormListItem>
+
+        <BasicTextarea
+          :error="state.counselingError"
+          titleText="상담신청내용"
+          title="상담신청내용"
+        />
+      </FormList>
+    </section>
+
+    <ButtonList>
+      <ButtonListItem>
+        <BasicButton>다음</BasicButton>
+      </ButtonListItem>
+    </ButtonList>
+
+    <!-- Case : 서비스 이용약관 내 [다음] 선택 시 노출 -->
+    <ButtonList :wrap="true">
+      <ButtonListItem>
+        <BasicButton line="true">대출관련 상담신청</BasicButton>
+      </ButtonListItem>
+      <ButtonListItem>
+        <BasicButton>민원접수</BasicButton>
+      </ButtonListItem>
+    </ButtonList>
+    <!-- // Case : 서비스 이용약관 내 [다음] 선택 시 노출 -->
   </PageContents>
 </template>
