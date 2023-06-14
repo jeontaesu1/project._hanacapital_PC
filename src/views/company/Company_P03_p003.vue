26881afdee007c47f3f0bb63c94d4abc2396eb95
sjkim03 <sojin@mwkorea.co.kr>
2023-06-14 20:00:43 +0900

    게시글 리스트/상세 화면 작업( 검색결과 없음 케이스 제외 )
    손님용 > 회사소개
    - 회사소개
       > IR정보
       > 지속가능경영
    - PR
       > 보도자료
       > 사회공헌
    - 투자정보 > 경영공시
    - 채용정보 > 채용공고
    
    영업용(에이전트 홈페이지)
    - 공지사항
    - 교육정보
    - 업무절차
    - 각종서식다운로드

--- src/views/company/Company_P03_p003.vue
+++ src/views/company/Company_P03_p003.vue
@@ -3,22 +3,135 @@
 import PageContents from '@/components/ui/layout/PageContents.vue';
 import PageHead from '@/components/ui/text/PageHead.vue';
 import PageTitle from '@/components/ui/text/PageTitle.vue';
+import InputBlock from '@/components/ui/form/InputBlock.vue';
+import InputBlockCell from '@/components/ui/form/InputBlockCell.vue';
+import BasicInput from '@/components/ui/form/BasicInput.vue';
+import SearchButton from '@/components/ui/button/SearchButton.vue';
+import PaginationNav from '@/components/ui/pagination/PaginationNav.vue';
+import PaginationNavArrow from '@/components/ui/pagination/PaginationNavArrow.vue';
+import PaginationNavEllipsis from '@/components/ui/pagination/PaginationNavEllipsis.vue';
+import PaginationNavNumber from '@/components/ui/pagination/PaginationNavNumber.vue';
 
 export default {
   components: {
     PageContents,
     PageHead,
     PageTitle,
+    InputBlock,
+    InputBlockCell,
+    BasicInput,
+    SearchButton,
+    PaginationNav,
+    PaginationNavArrow,
+    PaginationNavEllipsis,
+    PaginationNavNumber,
   },
 };
 </script>
 
 <template>
-  <PageContents>
+  <PageContents size="wide">
     <PageHead>
-      <PageTitle>타이틀</PageTitle>
+      <PageTitle>사회공헌</PageTitle>
     </PageHead>
 
-    // contents
+    <div :class="$style['board-search-bar']">
+      <InputBlock>
+        <InputBlockCell :flexible="true">
+          <BasicInput
+            type="search"
+            title="게시물 검색어"
+            placeholder="검색어 입력"
+          />
+        </InputBlockCell>
+        <InputBlockCell type="search">
+          <SearchButton />
+        </InputBlockCell>
+      </InputBlock>
+    </div>
+
+    <div>
+      <!-- table -->
+      <div :class="$style['board']">
+        <table>
+          <colgroup>
+            <col style="width: 67px" />
+            <col />
+            <col style="width: 120px" />
+          </colgroup>
+          <tbody>
+            <tr v-for="i in 10" :key="i">
+              <td>
+                <span class="color-black font-weight-light">{{ i }}</span>
+              </td>
+              <td>
+                <RouterLink
+                  to="/company/Company_P03_p004"
+                  :class="$style['board__link']"
+                >
+                  <span :class="[$style['board__link-text'], 'ellipsis']">
+                    게시물 제목
+                  </span>
+                </RouterLink>
+              </td>
+              <td>2022.08.30</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <!-- // table -->
+
+      <!-- Case : 첫번째 페이지일 때 -->
+      <PaginationNav>
+        <PaginationNavArrow type="prev" :disabled="true" />
+        <PaginationNavNumber :active="true">1</PaginationNavNumber>
+        <PaginationNavNumber>2</PaginationNavNumber>
+        <PaginationNavNumber>3</PaginationNavNumber>
+        <PaginationNavNumber>4</PaginationNavNumber>
+        <PaginationNavNumber>5</PaginationNavNumber>
+        <PaginationNavNumber>6</PaginationNavNumber>
+        <PaginationNavNumber>7</PaginationNavNumber>
+        <PaginationNavEllipsis />
+        <PaginationNavNumber>999</PaginationNavNumber>
+        <PaginationNavArrow type="next" />
+      </PaginationNav>
+      <!-- // Case : 첫번째 페이지일 때 -->
+
+      <!-- Case : 중간 페이지일 때 -->
+      <PaginationNav>
+        <PaginationNavArrow type="prev" />
+        <PaginationNavNumber>1</PaginationNavNumber>
+        <PaginationNavEllipsis />
+        <PaginationNavNumber>13</PaginationNavNumber>
+        <PaginationNavNumber>14</PaginationNavNumber>
+        <PaginationNavNumber :active="true">15</PaginationNavNumber>
+        <PaginationNavNumber>16</PaginationNavNumber>
+        <PaginationNavNumber>17</PaginationNavNumber>
+        <PaginationNavEllipsis />
+        <PaginationNavNumber>99</PaginationNavNumber>
+        <PaginationNavArrow type="next" />
+      </PaginationNav>
+      <!-- // Case : 중간 페이지일 때 -->
+
+      <!-- Case : 마지막 페이지일 때 -->
+      <PaginationNav>
+        <PaginationNavArrow type="prev" />
+        <PaginationNavNumber>1</PaginationNavNumber>
+        <PaginationNavEllipsis />
+        <PaginationNavNumber>93</PaginationNavNumber>
+        <PaginationNavNumber>94</PaginationNavNumber>
+        <PaginationNavNumber>95</PaginationNavNumber>
+        <PaginationNavNumber>96</PaginationNavNumber>
+        <PaginationNavNumber>97</PaginationNavNumber>
+        <PaginationNavNumber>98</PaginationNavNumber>
+        <PaginationNavNumber :active="true">99</PaginationNavNumber>
+        <PaginationNavArrow type="next" :disabled="true" />
+      </PaginationNav>
+      <!-- // Case : 마지막 페이지일 때 -->
+    </div>
   </PageContents>
 </template>
+
+<style lang="scss" module>
+@import '@/assets/scss/views/company/Company_P03_p003.scss';
+</style>
