92dc60ca0b3f37a3ca3355fd85aaf71782fc856e
yoondowon <yoondw7832@mwkorea.co.kr>
2023-06-20 10:42:10 +0900

    손님용- 오토금융
    모바일 진행 URL 전송 팝업, 상담요청 페이지 작업 완료

--- src/views/auto/AF_P01_l001.vue
+++ src/views/auto/AF_P01_l001.vue
@@ -1,13 +1,24 @@
 <script>
 // AF_P01_l001
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 
 import UiLayer from '@/components/ui/layer/UiLayer.vue';
 import PopupButton from '@/components/ui/layer/PopupButton.vue';
 import PopupTitle from '@/components/ui/layer/PopupTitle.vue';
-import PopupText from '@/components/ui/layer/PopupText.vue';
+import PopupSubTitle from '@/components/ui/layer/PopupSubTitle.vue';
+import FormList from '@/components/ui/form/FormList.vue';
+import FormListItem from '@/components/ui/form/FormListItem.vue';
+import FormInvalid from '@/components/ui/form/FormInvalid.vue';
+import InputBlock from '@/components/ui/form/InputBlock.vue';
+import InputBlockCell from '@/components/ui/form/InputBlockCell.vue';
+import BasicInput from '@/components/ui/form/BasicInput.vue';
+import FormInvalidMessage from '@/components/ui/form/FormInvalidMessage.vue';
 import ModalPopup from '@/components/ui/layer/ModalPopup.vue';
 import ModalPopupHead from '@/components/ui/layer/ModalPopupHead.vue';
+import CheckBox from '@/components/ui/form/CheckBox.vue';
+import CheckBoxLabelText from '@/components/ui/form/CheckBoxLabelText.vue';
+import CheckBoxObject from '@/components/ui/form/CheckBoxObject.vue';
+import BasicBox from '@/components/ui/common/BasicBox.vue';
 import BasicButton from '@/components/ui/button/BasicButton.vue';
 import ButtonList from '@/components/ui/button/ButtonList.vue';
 import ButtonListItem from '@/components/ui/button/ButtonListItem.vue';
@@ -17,17 +28,33 @@ export default {
     UiLayer,
     PopupButton,
     PopupTitle,
-    PopupText,
+    PopupSubTitle,
+    FormList,
+    FormListItem,
+    FormInvalid,
+    InputBlock,
+    InputBlockCell,
+    BasicInput,
+    FormInvalidMessage,
     ModalPopup,
     ModalPopupHead,
+    CheckBox,
+    CheckBoxLabelText,
+    CheckBoxObject,
+    BasicBox,
     BasicButton,
     ButtonList,
     ButtonListItem,
   },
   setup() {
+    const state = reactive({
+      phoneError: false,
+    });
+
     const layer = ref(null);
 
     return {
+      state,
       layer,
     };
   },
@@ -42,11 +69,67 @@ export default {
           <template v-slot:right>
             <PopupButton @click="layerSlotProps.close()" />
           </template>
-          <PopupTitle>타이틀</PopupTitle>
+          <PopupTitle>모바일로 상담을 진행하시겠어요 ?</PopupTitle>
+          <template v-slot:sub>
+            <PopupSubTitle>
+              입력하신 연락처로 모바일 상담 가능한 URL을 전송해 드립니다.
+            </PopupSubTitle>
+          </template>
         </ModalPopupHead>
       </template>
 
-      <PopupText>텍스트</PopupText>
+      <FormList>
+        <FormListItem titleText="휴대전화" target="#AF_P01_l001_phone">
+          <FormInvalid :error="state.phoneError">
+            <InputBlock :error="state.phoneError">
+              <InputBlockCell :flexible="true">
+                <BasicInput
+                  title="휴대전화"
+                  pattern="\d*"
+                  id="AF_P01_l001_phone"
+                />
+              </InputBlockCell>
+            </InputBlock>
+            <FormInvalidMessage>Error Message</FormInvalidMessage>
+          </FormInvalid>
+        </FormListItem>
+      </FormList>
+
+      <BasicBox className="row-margin-contents">
+        <div :class="$style['agree-list']">
+          <ul
+            :class="[
+              $style['agree-list__list'],
+              $style['agree-list__list--secondary'],
+              $style['agree-list__list--secondary-no-padding'],
+            ]"
+          >
+            <li :class="$style['agree-list__item']">
+              <div :class="$style['agree-list__head']">
+                <CheckBox
+                  id="PF_P07_p001_agree001"
+                  :classNames="{
+                    wrap: $style['agree-list__checkbox'],
+                  }"
+                  theme="tertiary"
+                >
+                  <CheckBoxObject />
+                  <CheckBoxLabelText>
+                    개인정보 수집·이용 동의
+                  </CheckBoxLabelText>
+                </CheckBox>
+                <div :class="$style['agree-list__right']">
+                  <button type="button" :class="$style['agree-list__link']">
+                    <span :class="$style['agree-list__link-text']">
+                      상세보기
+                    </span>
+                  </button>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </BasicBox>
 
       <template v-slot:foot>
         <ButtonList
@@ -57,7 +140,7 @@ export default {
           }"
         >
           <ButtonListItem>
-            <BasicButton size="regular">확인</BasicButton>
+            <BasicButton size="regular">전송하기</BasicButton>
           </ButtonListItem>
         </ButtonList>
       </template>
@@ -65,3 +148,6 @@ export default {
   </UiLayer>
 </template>
 
+<style lang="scss" module>
+@import '@/assets/scss/views/auto/AF_P01_l001.scss';
+</style>
--- src/views/uiGuide/GuideAuto001.vue
+++ src/views/uiGuide/GuideAuto001.vue
@@ -7,6 +7,7 @@ import ButtonList from '@/components/ui/button/ButtonList.vue';
 import ButtonListItem from '@/components/ui/button/ButtonListItem.vue';
 
 import AF_P01_l002 from '@/views/auto/AF_P01_l002.vue';
+import AF_P01_l001 from '@/views/auto/AF_P01_l001.vue';
 
 export default {
   components: {
@@ -15,17 +16,24 @@ export default {
     ButtonList,
     ButtonListItem,
     AF_P01_l002,
+    AF_P01_l001,
   },
   setup() {
     const layer001 = ref(null);
+    const layer002 = ref(null);
 
     const layer001Open = (e = {}) => {
       layer001.value.layer.open(e.target);
     };
+    const layer002Open = (e = {}) => {
+      layer002.value.layer.open(e.target);
+    };
 
     return {
       layer001,
+      layer002,
       layer001Open,
+      layer002Open,
     };
   },
 };
@@ -44,8 +52,14 @@ export default {
           리스/렌트 비교정보 팝업<br />AF_P01_l002
         </BasicButton>
       </ButtonListItem>
+      <ButtonListItem>
+        <BasicButton @click="layer002Open">
+          모바일 진행 URL 전송 팝업<br />AF_P01_l001
+        </BasicButton>
+      </ButtonListItem>
     </ButtonList>
 
     <AF_P01_l002 ref="layer001" />
+    <AF_P01_l001 ref="layer002" />
   </PageContents>
 </template>
