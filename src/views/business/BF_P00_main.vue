31407d78a5c1de781c77f13925fc93ed41da1ac0
yoondowon <yoondw7832@mwkorea.co.kr>
2023-06-30 13:18:05 +0900

    디자인 수정사항 반영
    이미지 교체 작업 완료

--- src/views/business/BF_P00_main.vue
+++ src/views/business/BF_P00_main.vue
@@ -7,9 +7,9 @@ import PageTitle from '@/components/ui/text/PageTitle.vue';
 import PageMainText from '@/components/ui/text/PageMainText.vue';
 import TextButton from '@/components/ui/button/TextButton.vue';
 
+import IconleaseStuff from '@/assets/images/icon/lease-stuff.svg?component';
 import IconMedical from '@/assets/images/icon/medical.svg?component';
-import IconGeHealthcare from '@/assets/images/icon/ge-healthcare.svg?component';
-import IconIndustrial from '@/assets/images/icon/industrial.svg?component';
+import IconGoodsLoan from '@/assets/images/icon/goods-loan.svg?component';
 
 export default {
   components: {
@@ -19,9 +19,9 @@ export default {
     PageTitle,
     PageMainText,
     TextButton,
+    IconleaseStuff,
     IconMedical,
-    IconGeHealthcare,
-    IconIndustrial,
+    IconGoodsLoan,
   },
 };
 </script>
@@ -30,13 +30,12 @@ export default {
   <PageContents size="wide">
     <PageHead>
       <PageHeadRow>
-        <PageTitle> 기업을 위한 최적의 자금 대출! </PageTitle>
+        <PageTitle>의료기·설비를 위한 최적의 자금 대출!</PageTitle>
         <PageMainText
           align="center"
           :classNames="{ wrap: 'text-title-2 font-weight-regular' }"
         >
-          기업 재무상태와 담보 종류에 맞춘 대출이 가능합니다.<br />
-          하나캐피탈 기업금융이 여러분의 성공비지니스를 지원해드립니다.
+          하나캐피탈 의료기·설비 대출이 여러분의 성공 비즈니스를 지원해드립니다.
         </PageMainText>
       </PageHeadRow>
     </PageHead>
@@ -46,12 +45,34 @@ export default {
         <li :class="$style['product-list__item']">
           <div :class="$style['product-list__block']">
             <div :class="$style['product-list__icon']">
+              <IconleaseStuff />
+            </div>
+            <h3 :class="$style['product-list__title']">일반리스</h3>
+            <p :class="$style['product-list__text']">
+              물건 장기대여 금융 상품!
+            </p>
+            <div :class="$style['product-list__link']">
+              <TextButton
+                theme="secondary"
+                :underline="true"
+                textSize="regular"
+                tagName="RouterLink"
+                to=""
+                :classNames="{ wrap: $style['product-list__link-button'] }"
+              >
+                바로가기
+              </TextButton>
+            </div>
+          </div>
+        </li>
+        <li :class="$style['product-list__item']">
+          <div :class="$style['product-list__block']">
+            <div :class="$style['product-list__icon']">
               <IconMedical />
             </div>
-            <h3 :class="$style['product-list__title']">의료기할부</h3>
+            <h3 :class="$style['product-list__title']">일반할부</h3>
             <p :class="$style['product-list__text']">
-              병/의원 및 의료법인대상<br />
-              의료기기, 의료소모품 구입자금 대출
+              의료장비, 의료소모품, 설비 구입 지원
             </p>
             <div :class="$style['product-list__link']">
               <TextButton
@@ -70,36 +91,11 @@ export default {
         <li :class="$style['product-list__item']">
           <div :class="$style['product-list__block']">
             <div :class="$style['product-list__icon']">
-              <IconGeHealthcare />
+              <IconGoodsLoan />
             </div>
-            <h3 :class="$style['product-list__title']">의료기리스</h3>
+            <h3 :class="$style['product-list__title']">내구재할부</h3>
             <p :class="$style['product-list__text']">
-              의료장비, 의료기관 전용<br />
-              금융리스
-            </p>
-            <div :class="$style['product-list__link']">
-              <TextButton
-                theme="secondary"
-                :underline="true"
-                textSize="regular"
-                tagName="RouterLink"
-                to=""
-                :classNames="{ wrap: $style['product-list__link-button'] }"
-              >
-                바로가기
-              </TextButton>
-            </div>
-          </div>
-        </li>
-        <li :class="$style['product-list__item']">
-          <div :class="$style['product-list__block']">
-            <div :class="$style['product-list__icon']">
-              <IconIndustrial />
-            </div>
-            <h3 :class="$style['product-list__title']">산업재리스</h3>
-            <p :class="$style['product-list__text']">
-              각종 시설, 기계 등<br />
-              산업설비 전문 리스
+              생활에 꼭 필요한 소비재도 이젠 할부로!
             </p>
             <div :class="$style['product-list__link']">
               <TextButton
