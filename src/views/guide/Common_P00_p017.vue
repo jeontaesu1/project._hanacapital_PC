689430a3c84c4134640f121bb09c1fd62f6498a3
choo jeong yeon <cjy23405@mwkorea.co.kr>
2023-07-17 18:43:53 +0900

    추가 화면 파일 생성

---
+++ src/views/guide/Common_P00_p017.vue
@@ -0,0 +1,26 @@
+<script>
+// Common_P00_p017
+import PageContents from '@/components/ui/layout/PageContents.vue';
+import PageHead from '@/components/ui/text/PageHead.vue';
+import PageTitle from '@/components/ui/text/PageTitle.vue';
+import PageMainText from '@/components/ui/text/PageMainText.vue';
+
+export default {
+  components: {
+    PageContents,
+    PageHead,
+    PageTitle,
+    PageMainText,
+  },
+};
+</script>
+
+<template>
+  <PageContents>
+    <PageHead>
+      <PageTitle>타이틀</PageTitle>
+    </PageHead>
+
+    // contents
+  </PageContents>
+</template>
