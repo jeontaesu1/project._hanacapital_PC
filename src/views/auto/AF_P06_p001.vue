f996598d09ec12f071bcf2a3bba1655aa1c38d61
yoondowon <yoondw7832@mwkorea.co.kr>
2023-07-18 14:46:10 +0900

    상품안내 디자인 변경 사항 반영

--- src/views/auto/AF_P06_p001.vue
+++ src/views/auto/AF_P06_p001.vue
@@ -690,6 +690,7 @@ export default {
         :class="[
           $style['icon-list'],
           $style['icon-list--wrap'],
+          $style['icon-list--col-3'],
           'row-margin-container-medium',
         ]"
       >
--- src/views/auto/AF_P07_p001.vue
+++ src/views/auto/AF_P07_p001.vue
@@ -1288,6 +1288,7 @@ export default {
         :class="[
           $style['icon-list'],
           $style['icon-list--wrap'],
+          $style['icon-list--col-3'],
           'row-margin-container-medium',
         ]"
       >
