119b9342826fe2375ce79bb3e89c98c712e64552
cheeze <cjy23405@gmail.com>
2023-09-01 15:49:54 +0900

    [DT-108] 카드 비밀번호 placeholder 삭제

--- src/views/identification/Common_P00_p003.vue
+++ src/views/identification/Common_P00_p003.vue
@@ -982,7 +982,6 @@ export default {
                     title="카드비밀번호 앞 2자리"
                     :afterDot="2"
                     :length="2"
-                    placeholder="◌◌"
                     id="Common_P00_p003_cardPassword"
                   />
                 </InputBlockCell>
