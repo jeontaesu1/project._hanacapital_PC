f949c5e5a8582388d376b0d0be81627a81fa1264
yoondowon <yoondw7832@mwkorea.co.kr>
2023-09-19 18:19:00 +0900

    [DT-209] [Agent_P02_l008] 신규팝업 추가

---
+++ src/views/agent/Agent_P02_l008.vue
@@ -0,0 +1,161 @@
+<script>
+// Agent_P02_l008
+import { ref } from 'vue';
+
+import UiLayer from '@/components/ui/layer/UiLayer.vue';
+import PopupTitle from '@/components/ui/layer/PopupTitle.vue';
+import PopupButton from '@/components/ui/layer/PopupButton.vue';
+import ModalPopup from '@/components/ui/layer/ModalPopup.vue';
+import ModalPopupHead from '@/components/ui/layer/ModalPopupHead.vue';
+import SimpleDatepicker from '@/components/ui/form/SimpleDatepicker.vue';
+import SimpleInput from '@/components/ui/form/SimpleInput.vue';
+import ButtonList from '@/components/ui/button/ButtonList.vue';
+import ButtonListItem from '@/components/ui/button/ButtonListItem.vue';
+import BasicButton from '@/components/ui/button/BasicButton.vue';
+
+export default {
+  components: {
+    UiLayer,
+    PopupTitle,
+    PopupButton,
+    ModalPopup,
+    ModalPopupHead,
+    SimpleDatepicker,
+    SimpleInput,
+    ButtonList,
+    ButtonListItem,
+    BasicButton,
+  },
+  setup() {
+    const layer = ref(null);
+
+    return {
+      layer,
+    };
+  },
+};
+</script>
+
+<template>
+  <UiLayer ref="layer" v-slot="layerSlotProps">
+    <ModalPopup size="large">
+      <template v-slot:head>
+        <ModalPopupHead>
+          <template v-slot:right>
+            <PopupButton @click="layerSlotProps.close()" />
+          </template>
+          <PopupTitle>민원세부정보</PopupTitle>
+        </ModalPopupHead>
+      </template>
+
+      <section>
+        <ButtonList align="right" :classNames="{ wrap: 'row-margin-contents' }">
+          <ButtonListItem>
+            <BasicButton size="small" :line="true">접수</BasicButton>
+          </ButtonListItem>
+          <ButtonListItem :class="$style['accept-btn']">
+            <BasicButton size="small" :line="true">처리중</BasicButton>
+          </ButtonListItem>
+          <ButtonListItem :class="$style['accept-btn']">
+            <BasicButton size="small">처리완료</BasicButton>
+          </ButtonListItem>
+        </ButtonList>
+
+        <div :class="[$style['basic-table'], $style['basic-table--secondary']]">
+          <table>
+            <colgroup>
+              <col style="width: 160px" />
+              <col />
+              <col style="width: 160px" />
+              <col />
+              <col style="width: 160px" />
+              <col />
+            </colgroup>
+            <tbody>
+              <tr>
+                <th class="align-left">성명(상호)</th>
+                <td class="align-left">김하나</td>
+                <th class="align-left">실행일자</th>
+                <td class="align-left">2022.07.14</td>
+                <th class="align-left">발생일</th>
+                <td class="align-left">2022.08.03</td>
+              </tr>
+              <tr>
+                <th class="align-left">대표자명</th>
+                <td class="align-left"></td>
+                <th class="align-left">품목</th>
+                <td class="align-left">LED 전광판</td>
+                <th class="align-left">이송일</th>
+                <td class="align-left">2022.08.03</td>
+              </tr>
+              <tr>
+                <th class="align-left">형태구분</th>
+                <td class="align-left">개인</td>
+                <th class="align-left">할부원금</th>
+                <td class="align-left">2,500,000</td>
+                <th class="align-left">접수일</th>
+                <td class="align-left">
+                  <!-- Case : 접수상태 -->
+                  Data
+                  <!--// Case : 접수상태 -->
+                </td>
+              </tr>
+              <tr>
+                <th class="align-left">주민등록번호</th>
+                <td class="align-left">123456-1******</td>
+                <th class="align-left">원금잔액</th>
+                <td class="align-left">2,500,000</td>
+                <th class="align-left">처리완료예정일</th>
+                <td class="align-left">
+                  <SimpleDatepicker
+                    title="처리 완료일"
+                    id="Agent_P02_l008_date"
+                    buttonId="Agent_P02_l008_date_Button"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th class="align-left">사업자번호</th>
+                <td class="align-left"></td>
+                <th class="align-left">잔여/할부기간</th>
+                <td class="align-left">30/30</td>
+                <th class="align-left">처리완료일</th>
+                <td class="align-left">
+                  <!-- Case : 처리완료상태 -->
+                  Data
+                  <!-- // Case : 처리완료상태 -->
+                </td>
+              </tr>
+              <tr>
+                <th class="align-left">담당자</th>
+                <td class="align-left">김용포</td>
+                <th class="align-left">민원유형</th>
+                <td class="align-left" colspan="3">신용도 영향</td>
+              </tr>
+              <tr>
+                <th class="align-left">민원내용</th>
+                <td colspan="5" class="align-left">신한프라자</td>
+              </tr>
+              <tr>
+                <th class="align-left">처래예정내용</th>
+                <td colspan="5" class="align-left">
+                  <SimpleInput placeholder="예정내용" />
+                </td>
+              </tr>
+              <tr>
+                <th class="align-left">처리내용</th>
+                <td colspan="5" class="align-left">
+                  <SimpleInput placeholder="확인 결과" />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
+    </ModalPopup>
+  </UiLayer>
+</template>
+
+<style lang="scss" module>
+@import '@/assets/scss/views/agent/Agent_P02_l008.scss';
+</style>
--- src/views/uiGuide/GuideAgent003.vue
+++ src/views/uiGuide/GuideAgent003.vue
@@ -7,6 +7,7 @@ import ButtonList from '@/components/ui/button/ButtonList.vue';
 import ButtonListItem from '@/components/ui/button/ButtonListItem.vue';
 
 import Agent_P02_l003 from '@/views/agent/Agent_P02_l003.vue';
+import Agent_P02_l008 from '@/views/agent/Agent_P02_l008.vue';
 import Agent_P00_l010 from '@/views/agent/Agent_P00_l010.vue';
 import Agent_P00_l011 from '@/views/agent/Agent_P00_l011.vue';
 import Agent_P02_l002 from '@/views/agent/Agent_P02_l002.vue';
@@ -22,6 +23,7 @@ export default {
     ButtonList,
     ButtonListItem,
     Agent_P02_l003,
+    Agent_P02_l008,
     Agent_P00_l010,
     Agent_P00_l011,
     Agent_P02_l002,
@@ -39,6 +41,7 @@ export default {
     const layer006 = ref(null);
     const layer007 = ref(null);
     const layer008 = ref(null);
+    const layer009 = ref(null);
 
     const layer001Open = (e = {}) => {
       layer001.value.layer.open(e.target);
@@ -64,6 +67,9 @@ export default {
     const layer008Open = (e = {}) => {
       layer008.value.layer.open(e.target);
     };
+    const layer009Open = (e = {}) => {
+      layer009.value.layer.open(e.target);
+    };
 
     return {
       layer001,
@@ -74,6 +80,7 @@ export default {
       layer006,
       layer007,
       layer008,
+      layer009,
       layer001Open,
       layer002Open,
       layer003Open,
@@ -82,6 +89,7 @@ export default {
       layer006Open,
       layer007Open,
       layer008Open,
+      layer009Open,
     };
   },
 };
@@ -102,48 +110,54 @@ export default {
       </ButtonListItem>
       <ButtonListItem>
         <BasicButton @click="layer002Open">
-          판매대리점조회 팝업<br />Agent_P00_l010
+          민원세부정보 팝업<br />Agent_P02_l008
         </BasicButton>
       </ButtonListItem>
       <ButtonListItem>
         <BasicButton @click="layer003Open">
-          품목선택 팝업<br />Agent_P00_l011
+          판매대리점조회 팝업<br />Agent_P00_l010
         </BasicButton>
       </ButtonListItem>
       <ButtonListItem>
         <BasicButton @click="layer004Open">
-          상환스케쥴시뮬레이터 팝업<br />Agent_P02_l002
+          품목선택 팝업<br />Agent_P00_l011
         </BasicButton>
       </ButtonListItem>
       <ButtonListItem>
         <BasicButton @click="layer005Open">
-          본인확인(본의명의휴대폰소지자) 팝업<br />Agent_P02_l004
+          상환스케쥴시뮬레이터 팝업<br />Agent_P02_l002
         </BasicButton>
       </ButtonListItem>
       <ButtonListItem>
         <BasicButton @click="layer006Open">
-          하나캐피탈 전자 약정 URL 발송 팝업<br />Agent_P02_l005
+          본인확인(본의명의휴대폰소지자) 팝업<br />Agent_P02_l004
         </BasicButton>
       </ButtonListItem>
       <ButtonListItem>
         <BasicButton @click="layer007Open">
-          판매점 제휴 협약을 위한 개인(신용)정보동의 URL 발송 팝업<br />Agent_P02_l006
+          하나캐피탈 전자 약정 URL 발송 팝업<br />Agent_P02_l005
         </BasicButton>
       </ButtonListItem>
       <ButtonListItem>
         <BasicButton @click="layer008Open">
+          판매점 제휴 협약을 위한 개인(신용)정보동의 URL 발송 팝업<br />Agent_P02_l006
+        </BasicButton>
+      </ButtonListItem>
+      <ButtonListItem>
+        <BasicButton @click="layer009Open">
           하나캐피탈 할부 상담 URL 발송<br />Agent_P02_l007
         </BasicButton>
       </ButtonListItem>
     </ButtonList>
 
     <Agent_P02_l003 ref="layer001" />
-    <Agent_P00_l010 ref="layer002" />
-    <Agent_P00_l011 ref="layer003" />
-    <Agent_P02_l002 ref="layer004" />
-    <Agent_P02_l004 ref="layer005" />
-    <Agent_P02_l005 ref="layer006" />
-    <Agent_P02_l006 ref="layer007" />
-    <Agent_P02_l007 ref="layer008" />
+    <Agent_P02_l008 ref="layer002" />
+    <Agent_P00_l010 ref="layer003" />
+    <Agent_P00_l011 ref="layer004" />
+    <Agent_P02_l002 ref="layer005" />
+    <Agent_P02_l004 ref="layer006" />
+    <Agent_P02_l005 ref="layer007" />
+    <Agent_P02_l006 ref="layer008" />
+    <Agent_P02_l007 ref="layer009" />
   </PageContents>
 </template>
