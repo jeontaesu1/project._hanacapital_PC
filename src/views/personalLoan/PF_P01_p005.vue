8eb8e49d0cf95578a531bfa9b1a014a1755c8901
yoondowon <yoondw7832@mwkorea.co.kr>
2023-09-25 16:19:35 +0900

    [DT-230] 개인대출 한도조회 신청정보입력 금액 한글 표시

--- src/views/personalLoan/PF_P01_p005.vue
+++ src/views/personalLoan/PF_P01_p005.vue
@@ -692,6 +692,13 @@ export default {
                 </template>
               </InputBlock>
               <FormInvalidMessage>Error Message</FormInvalidMessage>
+
+              <!-- Case : 대출신청금액 입력 시 노출 -->
+              <FormHelpText :classNames="{ wrap: 'align-right' }">
+                2억원
+              </FormHelpText>
+              <!-- // Case : 대출신청금액 입력 시 노출 -->
+
               <NoticeText
                 :classNames="{
                   wrap: 'row-margin-item-medium',
