c92bb4e1607da8926a82aab99316b292434e69d7
yoondowon <yoondw7832@mwkorea.co.kr>
2023-10-26 14:11:06 +0900

    메인 팝업

--- src/views/main/MainHome.vue
+++ src/views/main/MainHome.vue
@@ -26,6 +26,9 @@ import BoxCheckListItem from '@/components/ui/form/BoxCheckListItem.vue';
 import BasicButton from '@/components/ui/button/BasicButton.vue';
 import ButtonList from '@/components/ui/button/ButtonList.vue';
 import ButtonListItem from '@/components/ui/button/ButtonListItem.vue';
+import ModalPopup from '@/components/ui/layer/ModalPopup.vue';
+import ModalPopupHead from '@/components/ui/layer/ModalPopupHead.vue';
+import PopupTitle from '@/components/ui/layer/PopupTitle.vue';
 
 import IconAdd from '@/assets/images/icon/add.svg?component';
 import IconLink from '@/assets/images/icon/link.svg?component';
@@ -77,6 +80,9 @@ export default {
     BasicButton,
     ButtonList,
     ButtonListItem,
+    ModalPopup,
+    ModalPopupHead,
+    PopupTitle,
 
     IconAdd,
     IconLink,
@@ -1258,8 +1264,50 @@ export default {
       </div>
     </div>
   </PageContents>
+
+  <!-- 팝업 -->
+  <div :class="$style['popup']">
+    <ModalPopup line="secondary">
+      <template v-slot:head>
+        <ModalPopupHead>
+          <PopupTitle>금융사기 피해예방 안내문</PopupTitle>
+        </ModalPopupHead>
+      </template>
+
+      <div :class="$style['image-view']">
+        <img src="@/assets/images/_dummy/img-notice.png" alt="샘플 이미지" />
+      </div>
+
+      <template v-slot:bgBottom>
+        <div class="flex-box">
+          <div class="flex-box__cell flex-1">
+            <TextButton
+              :underline="true"
+              :classNames="{
+                wrap: 'text-body-3 color-white',
+              }"
+            >
+              오늘 하루 보지 않기
+            </TextButton>
+          </div>
+          <div class="flex-box__cell">
+            <TextButton
+              :underline="true"
+              :classNames="{
+                wrap: 'text-title-2 color-white',
+              }"
+            >
+              닫기
+            </TextButton>
+          </div>
+        </div>
+      </template>
+    </ModalPopup>
+  </div>
+  <!-- // 팝업 -->
 </template>
 
 <style lang="scss" module>
 @import '@/assets/scss/views/main/MainHome.scss';
+@import '@/assets/scss/views/main/Main_P01_l002.scss';
 </style>
