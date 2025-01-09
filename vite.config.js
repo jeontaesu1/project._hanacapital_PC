3448bae209d48f507777e3987a15742a6b8fe404
jeontaesu1 <jts@frameout.co.kr>
2025-01-09 15:17:41 +0900

    깃 액션테스트4

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:
-    process.env.NODE_ENV === 'production' ? '/project._hanacapital_PC/' : '/',
+  base: '/project._hanacapital_PC/',
 
   plugins: [
     vue({
